6e4e06436c3f584a773c8437a488559cd9b0d1ca
Yu Chi To <yuchitoto@gmail.com>
2020-05-31 23:54:04 +0800

    game.py loader

---
+++ src/game.py
@@ -0,0 +1,37 @@
+"""
+Game class for holding mahjong game
+"""
+
+#from player import Player
+
+class Game:
+    def __init__(self, dir_path):
+        self.tiles = load_tiles(dir_path) # load all tiles from file for different mahjong
+        self.num_tiles = len(self.tiles)
+        self.hands = load_hands(dir_path) # load all hands from file for different mahjong
+        self.players = []
+
+
+    def load_tiles(self, dir_path):
+        # load tiles file
+        path = dir_path + '/tiles.txt'
+        with open(path, 'rt') as file:
+            content = file.read()
+
+        # split string to get all tiles
+        content = content.split(' |,|\t|\n')
+        return content
+
+
+    def load_hands(self, dir_path):
+        # load hands file
+        path = dir_path + '/hands.txt'
+        with open(path, 'rt') as file:
+            content = file.readlines()
+
+        # split to form list of hands
+        for i in range(len(content)):
+            content[i] = content[i].split(' |,|\t')
+            content[i] = {"hand":content[i][0], "pattern": content[i][1:]}
+
+        return content
